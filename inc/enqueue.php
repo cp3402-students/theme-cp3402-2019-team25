9219c3818b531b4956fa92d03ddc8fbbbe058a69
Jack.Bonnell <jackebonnell@gmail.com>
2019-05-28 13:23:24 +1000

    Change padding values on header list elements to %

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -25,10 +25,17 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
 		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
+		wp_enqueue_script( 'understrap-theme-scripts', get_template_directory_uri() . '/js/resize.js', false,  NULL, true);
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
 	}
-} // endif function_exists( 'understrap_scripts' ).
+	
+	function customScripts() {
 
+		wp_enqueue_script( 'resize', get_template_directory_uri() . '/js/resize.js');
+	}
+	
+} // endif function_exists( 'understrap_scripts' ).
 add_action( 'wp_enqueue_scripts', 'understrap_scripts' );
+add_action( 'wp_enqueue_scripts', 'customScripts' );
